256c994032af06a0eaa06353ce40225bbc1e2b79
BarenneM <barenne.marie@outlook.com>
2020-06-29 11:26:07 +0200

    'Classe Stagiaire finie'

--- src/metier/Stagiaire.java
+++ src/metier/Stagiaire.java
@@ -2,4 +2,11 @@ package metier;
 
 public class Stagiaire {
 
+	private String nom;
+	private String prenom;
+	
+	public Stagiaire(String nom, String prenom) {
+		this.nom = nom;
+		this.prenom = prenom;
+	}
 }
